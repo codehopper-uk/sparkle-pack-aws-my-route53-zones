aa40ef098c248c4ec7b113904d2808e984ea4f8d
Greg Swallow <gswallow@gmail.com>
2016-12-07 10:20:42 -0500

    ERROR: RuntimeError: can't modify frozen String

--- lib/sparkleformation/registry/my-route53-zones.rb
+++ lib/sparkleformation/registry/my-route53-zones.rb
@@ -6,6 +6,6 @@ route53 = ::Aws::Route53::Client.new
 zones = route53.list_hosted_zones.hosted_zones
 
 SfnRegistry.register(:my_hosted_zone) do |filter|
-  filter << '.' unless filter.end_with?('.')
-  zones.collect { |z| z.id.delete('/hostedzone/') if z.name == filter }.compact.first
+  name = filter.end_with?('.') ? filter : "#{filter}."
+  zones.collect { |z| z.id.delete('/hostedzone/') if z.name == name }.compact.first
 end
